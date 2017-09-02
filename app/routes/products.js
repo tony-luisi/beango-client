9d29dfc311d365a7283acc2d3bce2338eb5e8540
Tony Luisi <t.luisi@gmail.com>
2017-09-02 22:52:42 +1200

    products showing

--- app/routes/products.js
+++ app/routes/products.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return [{
-      id: 1,
-      name: 'Flat White',
-      price: 4.5,
-      size: 'small'
-    }]
+    return this.get('store').findAll('product')
   }
 });
